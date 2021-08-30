030fb234bd85e5c8cfc6a914d52b13b8877bdc0a
“Yang-macbook” <20212010022@fudan.edu.cn>
2021-08-30 10:10:49 +0800

    [U] 修改github actions 文件

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
